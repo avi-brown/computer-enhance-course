7b0a78ec62814f3138c69ee1c0d31bc46ef9e375
avi-brown <me@avi.engineer>
2025-12-14 22:29:29 +0200

    basic decoder stuff

--- part1/8086_decoder.py
+++ part1/8086_decoder.py
@@ -1,25 +1,49 @@
 import sys
 sys.path.append("..")
-from print_utils import pretty_print
+from print_utils import pretty_print, print_bin
 
 class Decoder_8086:
     def __init__(self, path):
         self.path = path
-        self.current_op = None
+
+        self.op = None
+        self.d = None
+        self.w = None
+        self.mod = None
+        self.reg = None
+        self.rm = None
+
         self.opcodes = {
-            100010: 'mov',
+            0b100010: 'mov',
         }
 
-        @pretty_print
-        def decode(self):    
-            FILE = open(self.path, 'rb')
-            raw_bytes_data = FILE.read()
-            bin_data = ''.join(f'{byte:08b}' for byte in raw_bytes_data)
-            print("READ FILE")
-            print("GOT BINARY DATA: ", bin_data)
+    @pretty_print
+    def decode_op(self):    
+        FILE = open(self.path, 'rb')
+        raw_bytes_data = FILE.read()
+        bin_data = ''.join(f'{byte:08b}' for byte in raw_bytes_data)
+        print("READ FILE")
+        print("GOT BINARY DATA: ", bin_data)
 
-            self.current_op = self.opcodes[bin_data[:6]]
-            print(self.current_op)
+        self.op = self.opcodes[int(bin_data[:6], 2)]
+        self.d = int(bin_data[6], 2)
+        self.w = int(bin_data[7], 2)
+        self.mod = int(bin_data[8:10], 2)
+        self.reg = int(bin_data[10:13], 2)
+        self.rm = int(bin_data[13:16], 2)
+
+        print(self.op)
+        print_bin(self.d)
+        print_bin(self.w)
+        print_bin(self.mod)
+        print_bin(self.reg)
+        print_bin(self.rm)
+
+    @pretty_print
+    def print_current_op(self):
+        print("CURRENT OP: ", self.op)
 
 if __name__ == "__main__":
-    decode(sys.argv[1])
+    decoder = Decoder_8086(sys.argv[1])
+    decoder.decode_op()
+    decoder.print_current_op()
